a131a0a358bcfdc3342317480caea6af2cb46f8b
Preeti Meena <80210264+preeti-me@users.noreply.github.com>
2024-06-18 16:25:02 +0530

    Add files via upload

---
+++ sunrgbd_graph_data.m
@@ -0,0 +1,9 @@
+%clc;clear;close all;
+
+
+% buildingdata.building9.rooms = rmfield(buildingdata.building9.rooms, 'Adj_closestdistance_cntrd');
+% buildingdata.building9.rooms = rmfield(buildingdata.building9.rooms, 'normalizedAdj');
+% buildingdata.building9.rooms = rmfield(buildingdata.building9.rooms, 'bin_normalizedAdj');
+% buildingdata.building9.rooms = rmfield(buildingdata.building9.rooms, 'mvgraph');
+% buildingdata.building9.rooms = rmfield(buildingdata.building9.rooms, 'newmvmergraph');
+% buildingdata.building9.rooms = rmfield(buildingdata.building9.rooms, 'mvmergraph');
